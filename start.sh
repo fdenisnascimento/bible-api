f37c0bff4f8b4dd80f9ee82716c5a7c8e8dca52f
Darwin mac mini <darwinmacmini@darwinmacmini.local>
2023-10-17 08:49:18 -0300

    new release

--- start.sh
+++ start.sh
@@ -1,11 +1,3 @@
-# export DATABASE_HOST=52.0.118.216
-# export DATABASE_USERNAME=bible
-# export DATABASE_PASSWORD=bible@123456
-# export DATABASE_NAME=bible-db
-# export DATABASE_PORT=3808
-# export PORT=3040
-
-
 export DATABASE_HOST=localhost
 export DATABASE_USERNAME=root
 export DATABASE_PASSWORD=654321
